8e9007b241b95843496e103b5ec436b8fb918de6
piedone@pyrocenter.hu <piedone@pyrocenter.hu>
2011-10-29 12:27:25 +0200

    CreateTableCommand extensions

---
+++ Extensions/CreateTableCommandExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Orchard.Data.Migration.Schema;
+
+namespace Associativy.Extensions
+{
+    public static class CreateTableCommandExtensions
+    {
+        public static CreateTableCommand NodeConnectorRecord(this CreateTableCommand table)
+        {
+            table
+                .Column<int>("Id", column => column.PrimaryKey().Identity())
+                .Column<int>("Record1Id", column => column.NotNull())
+                .Column<int>("Record2Id", column => column.NotNull());
+
+            return table;
+        }
+
+        public static CreateTableCommand NodePartRecord(this CreateTableCommand table)
+        {
+            table
+                .ContentPartRecord()
+                .Column<string>("Label", column => column.NotNull());
+
+            return table;
+        }
+    }
+}
