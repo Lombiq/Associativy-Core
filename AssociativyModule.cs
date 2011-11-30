d0c328d753635b5e96a4c467637bdc1e7b5ccc51
piedone@pyrocenter.hu <piedone@pyrocenter.hu>
2011-11-30 18:13:23 +0100

    Moving dependency registration code to module

---
+++ AssociativyModule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Autofac.Core;
+using Autofac;
+using Associativy.Services;
+
+namespace Associativy
+{
+    public class AssociativyModule : IModule
+    {
+        public void Configure(IComponentRegistry componentRegistry)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterGeneric(typeof(AssociativyServices<,,>)).As(typeof(IAssociativyServices<,,>)).InstancePerLifetimeScope();
+            builder.RegisterGeneric(typeof(ConnectionManager<,,>)).As(typeof(IConnectionManager<,,>)).InstancePerLifetimeScope();
+            builder.RegisterGeneric(typeof(Mind<,,>)).As(typeof(IMind<,,>)).InstancePerLifetimeScope();
+            builder.RegisterGeneric(typeof(NodeManager<,>)).As(typeof(INodeManager<,>)).InstancePerLifetimeScope();
+
+            builder.Update(componentRegistry);
+        }
+    }
+}
