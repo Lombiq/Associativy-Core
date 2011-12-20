a8a18c355ccfe5c0c449a1ad9b69e12e3d599a38
piedone@pyrocenter.hu <piedone@pyrocenter.hu>
2011-12-20 19:23:56 +0100

    IAssociativyServices comments

--- Services/IAssociativyServices.cs
+++ Services/IAssociativyServices.cs
@@ -5,6 +5,12 @@ using Orchard.ContentManagement.Records;
 
 namespace Associativy.Services
 {
+    /// <summary>
+    /// Service collector for Associativy services
+    /// </summary>
+    /// <typeparam name="TNodePart">Content part type for nodes</typeparam>
+    /// <typeparam name="TNodePartRecord">Content part record type for nodes</typeparam>
+    /// <typeparam name="TNodeToNodeConnectorRecord">Record type for node to node connectors</typeparam>
     public interface IAssociativyServices<TNodePart, TNodePartRecord, TNodeToNodeConnectorRecord> : IDependency
         where TNodePart : ContentPart<TNodePartRecord>, INode
         where TNodePartRecord : ContentPartRecord, INode
