25a15eb9f68c5376bc80f79f0ef427d48c3f6301
piedone@pyrocenter.hu <piedone@pyrocenter.hu>
2011-12-20 19:50:53 +0100

    INode comment

--- Models/INode.cs
+++ Models/INode.cs
@@ -1,7 +1,7 @@
 ﻿namespace Associativy.Models
 {
     /// <summary>
-    /// Used mainly to mark NodeParts
+    /// Used mainly to mark NodeParts.
     /// 
     /// Very useful in INodeParams, as it makes it possible not to have to specify a TRecord too, if the constraint would be : NodePart
     /// </summary>
