792c3f6a7d88cbc717f7e498783751343c181ac2
piedone@pyrocenter.hu <piedone@pyrocenter.hu>
2011-12-21 20:36:52 +0100

    IFrontendEngineDriverLocator comments

--- FrontendEngines/IFrontendEngineDriverLocator.cs
+++ FrontendEngines/IFrontendEngineDriverLocator.cs
@@ -4,9 +4,18 @@ using Associativy.Models;
 
 namespace Associativy.FrontendEngines
 {
+    /// <summary>
+    /// Manages frontend engine (like Dracula or JIT) driver implementations
+    /// </summary>
+    /// <typeparam name="TNode">Node type</typeparam>
     public interface IFrontendEngineDriverLocator<TNode> : IDependency
         where TNode : INode
     {
+        /// <summary>
+        /// Returns the frontend engine driver specified by its name
+        /// </summary>
+        /// <param name="frontendEngineName">The frontend engine's name</param>
+        /// <returns>The frontend engine driver</returns>
         IFrontendEngineDriver<TNode> GetDriver(string frontendEngineName);
     }
 }
