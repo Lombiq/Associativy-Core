4ed61ddcf4f8937f96f1b5434daf4e4548b80e7d
piedone@pyrocenter.hu <piedone@pyrocenter.hu>
2011-12-21 21:35:01 +0100

    JIT driver

---
+++ FrontendEngines/Engines/JIT/JITDriver.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Orchard;
+using Orchard.DisplayManagement;
+using Associativy.FrontendEngines;
+using Orchard.Environment.Extensions;
+using Associativy.FrontendEngines.ViewModels;
+using Orchard.ContentManagement;
+using QuickGraph;
+using Associativy.Models;
+
+namespace Associativy.FrontendEngines.Engines.JIT
+{
+    [OrchardFeature("Associativy")]
+    public class JITDriver<TNode> : FrontendEngineDriver<TNode>
+        where TNode : INode
+    {
+        protected override string Name
+        {
+            get { return "JIT"; }
+        }
+
+        public JITDriver(
+            IOrchardServices orchardServices,
+            IShapeFactory shapeFactory,
+            IWorkContextAccessor workContextAccessor)
+            : base(orchardServices, shapeFactory, workContextAccessor)
+        {
+        }
+    }
+}
--- FrontendEngines/Engines/JIT/JITGraphNodeViewModel.cs
+++ FrontendEngines/Engines/JIT/JITGraphNodeViewModel.cs
@@ -1,29 +1,36 @@
 ﻿using System.Collections.Generic;
 using Associativy.Models;
+using Associativy.FrontendEngines.ViewModels;
+using Orchard.Environment.Extensions;
 
-namespace Associativy.ViewModels
+namespace Associativy.FrontendEngines.Engines.JIT.ViewModels
 {
-    //public abstract class JITGraphNodeViewModel<TNode> : GraphNodeViewModel<TNode>
-    //    where TNode : INode
-    //{
-    //    public int id
-    //    {
-    //        get { return Id; }
-    //        set { Id = value; }
-    //    }
+    /// <summary>
+    /// View model for JIT graphs. Naming follows JIT namings!
+    /// </summary>
+    /// <typeparam name="TNode"></typeparam>
+    [OrchardFeature("Associativy")]
+    public class JITGraphNodeViewModel<TNode> : GraphNodeViewModel<TNode>
+        where TNode : INode
+    {
+        public int id
+        {
+            get { return Id; }
+            set { Id = value; }
+        }
 
-    //    public string name
-    //    {
-    //        get { return Label; }
-    //        set { Label = value; }
-    //    }
+        public string name
+        {
+            get { return Label; }
+            set { Label = value; }
+        }
 
-    //    public IList<int> adjacencies
-    //    {
-    //        get { return NeighbourIds; }
-    //        set { NeighbourIds = value; }
-    //    }
+        public IList<int> adjacencies
+        {
+            get { return NeighbourIds; }
+            set { NeighbourIds = value; }
+        }
 
-    //    public IDictionary<string, string> data { get; set; }
-    //}
+        public IDictionary<string, string> data { get; set; }
+    }
 }
