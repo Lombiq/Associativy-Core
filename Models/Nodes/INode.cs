20a2893c760175642b68cbfe9a1e3549dade491f
piedone@pyrocenter.hu <piedone@pyrocenter.hu>
2011-12-30 00:55:09 +0100

    Relocating node models to subfolder

--- Models/Nodes/INode.cs
+++ Models/Nodes/INode.cs
@@ -3,7 +3,7 @@
     /// <summary>
     /// Used mainly to mark NodeParts.
     /// 
-    /// Very useful in INodeParams, as it makes it possible not to have to specify a TRecord too, if the constraint would be : NodePart
+    /// Very useful e.g. in INodeParams, as it makes it possible not to have to specify a TRecord too, if the constraint would be : NodePart
     /// </summary>
     public interface INode
     {
