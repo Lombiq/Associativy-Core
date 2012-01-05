fd7163b9ccf9e5b11d6118f44cffb8f0edba9a5d
piedone@pyrocenter.hu <piedone@pyrocenter.hu>
2012-01-05 14:35:36 +0100

    For the previous

---
+++ Services/IAssociativyService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Associativy.Models;
+
+namespace Associativy.Services
+{
+    public interface IAssociativyService
+    {
+        /// <summary>
+        /// The AssociativyContext the services use
+        /// </summary>
+        IAssociativyContext Context { get; set; }
+    }
+}
