5a70cf245ecbfb749b128d60d3ef8cf6b0f1aa25
piedone@pyrocenter.hu <piedone@pyrocenter.hu>
2012-01-13 15:51:24 +0100

    Separating frontend to other module

--- Scripts/FrontendEngines/Engines/Graphviz/jquery.mapquery.imageLayer.js
+++
@@ -1,9 +0,0 @@
-$.extend($.MapQuery.Layer.types, {
-    image: function (options) {
-        //delete options["type"];
-        return {
-            layer: new OpenLayers.Layer.Image(options["name"], options["url"], options["extent"], options["size"], options["options"]),
-            options: options
-        };
-    }
-});
