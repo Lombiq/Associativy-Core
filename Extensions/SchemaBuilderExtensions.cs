73b0b28de910cad55bf1c8aa7a4adb4aff813dff
piedone@pyrocenter.hu <piedone@pyrocenter.hu>
2012-01-14 13:07:44 +0100

    Code styling

--- Extensions/SchemaBuilderExtensions.cs
+++ Extensions/SchemaBuilderExtensions.cs
@@ -19,14 +19,12 @@ namespace Associativy.Extensions
 
             // TODO: TEST INDICES as data grows
             schemaBuilder.AlterTable(typeof(TNodeToNodeConnectorRecord).Name,
-                    table =>
-                    {
-                        table.CreateIndex("Connection", new string[] { "Node1Id", "Node2Id" });
-                        // These are maybe not needed
-                        // SELECT this_.Id as Id0_0_, this_.Record1Id as Record2_0_0_, this_.Record2Id as Record3_0_0_ FROM Associativy_Notions_NotionToNotionConnectorRecord this_ WHERE this_.Record1Id = 22
-                        //table.CreateIndex("Record1", new string[] { "Record1Id" });
-                        //table.CreateIndex("Record2", new string[] { "Record2Id" });
-                    }
+                table => table
+                    .CreateIndex("Connection", new string[] { "Node1Id", "Node2Id" })
+                    // These are maybe not needed
+                    // SELECT this_.Id as Id0_0_, this_.Record1Id as Record2_0_0_, this_.Record2Id as Record3_0_0_ FROM Associativy_Notions_NotionToNotionConnectorRecord this_ WHERE this_.Record1Id = 22
+                    //CreateIndex("Record1", new string[] { "Record1Id" });
+                    //CreateIndex("Record2", new string[] { "Record2Id" });
                 );
 
             return schemaBuilder;
