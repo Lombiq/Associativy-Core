8a5cc37b485948da0fc63a75ba2d00639284d746
piedone@pyrocenter.hu <piedone@pyrocenter.hu>
2012-01-17 23:12:02 +0100

    For the previous

---
+++ Models/IGraphDescriptor.cs
@@ -0,0 +1,33 @@
+﻿using Associativy.Services;
+using Orchard;
+using Orchard.Localization;
+
+namespace Associativy.Models
+{
+    /// <summary>
+    /// Describes the graph in which Associativy services are run, i.e. it stores information about the purpose and database
+    /// of associations
+    /// </summary>
+    public interface IGraphDescriptor : IDependency
+    {
+        /// <summary>
+        /// Human-readable name of the graph.
+        /// </summary>
+        LocalizedString GraphName { get; }
+
+        /// <summary>
+        /// Name of the graph used to identify it. Must be unique across the registered graphDescriptors.
+        /// </summary>
+        string TechnicalGraphName { get; }
+
+        /// <summary>
+        /// The types of the content items stored by the graph.
+        /// </summary>
+        string[] ContentTypes { get; }
+
+        /// <summary>
+        /// The IConnectionManager instance used by the graphDescriptor.
+        /// </summary>
+        IConnectionManager ConnectionManager { get; }
+    }
+}
---
+++ Models/ReadonlyGraphDescriptorBase.cs
@@ -0,0 +1,20 @@
+﻿using Associativy.Services;
+using Orchard.Environment.Extensions;
+using Orchard.Localization;
+using Piedone.HelpfulLibraries.DependencyInjection;
+
+namespace Associativy.Models
+{
+    [OrchardFeature("Associativy")]
+    public abstract class ReadonlyGraphDescriptorBase<TNodeToNodeConnectorRecord> : GraphDescriptorBase<TNodeToNodeConnectorRecord>
+        where TNodeToNodeConnectorRecord : INodeToNodeConnectorRecord, new()
+    {
+        public Localizer T { get; set; }
+
+        public ReadonlyGraphDescriptorBase(IResolve<IConnectionManager<TNodeToNodeConnectorRecord>> connectionManagerResolver)
+            : base(connectionManagerResolver)
+        {
+            T = NullLocalizer.Instance;
+        }
+    }
+}
