9237fdade572b5af357bab9817db07adc716c1f6
piedone@pyrocenter.hu <piedone@pyrocenter.hu>
2012-02-06 19:57:39 +0100

    NodeLabelPart import/export

--- Drivers/AssociativyNodeLabelPartDriver.cs
+++ Drivers/AssociativyNodeLabelPartDriver.cs
@@ -6,12 +6,22 @@ using Associativy.Models;
 namespace Associativy.Drivers
 {
     [OrchardFeature("Associativy")]
-    // This driver's sole purpose is to enable "casting" (.As<>()) of nodes to AssociativyNodeLabelPart
+    // This driver's purpose is also to enable "casting" (.As<>()) of nodes to AssociativyNodeLabelPart
     public class AssociativyNodeLabelPartDriver : ContentPartDriver<AssociativyNodeLabelPart>
     {
         protected override string Prefix
         {
             get { return "Associativy.NodeLabelPart"; }
+        }
+
+        protected override void Exporting(AssociativyNodeLabelPart part, Orchard.ContentManagement.Handlers.ExportContentContext context)
+        {
+            context.Element(part.PartDefinition.Name).SetAttributeValue("Label", part.Label);
+        }
+
+        protected override void Importing(AssociativyNodeLabelPart part, Orchard.ContentManagement.Handlers.ImportContentContext context)
+        {
+            part.Label = context.Attribute(part.PartDefinition.Name, "Label");
         }
     }
 }
