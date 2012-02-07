7e737c2cba2e1a10a127a444cb88686e55a357ca
piedone@pyrocenter.hu <piedone@pyrocenter.hu>
2012-02-07 15:12:44 +0100

    GraphContext.Stringify()

---
+++ GraphDiscovery/GraphContextExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Associativy.GraphDiscovery
+{
+    public static class GraphContextExtensions
+    {
+        public static string Stringify(this IGraphContext graphContext)
+        {
+            return graphContext.GraphName + String.Join(" ", graphContext.ContentTypes);
+        }
+    }
+}
