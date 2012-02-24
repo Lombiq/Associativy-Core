73f638fb1d45742c73d45b785fb3eeca5b1cc513
piedone@pyrocenter.hu <piedone@pyrocenter.hu>
2012-02-24 16:05:41 +0100

    GraphContextExtensions to the same file with IGraphContext

--- GraphDiscovery/IGraphContext.cs
+++ GraphDiscovery/IGraphContext.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Orchard.Environment.Extensions;
 
 namespace Associativy.GraphDiscovery
 {
@@ -22,5 +23,14 @@ namespace Associativy.GraphDiscovery
         /// The types of the content items stored by the graph
         /// </summary>
         IEnumerable<string> ContentTypes { get; }
+    }
+
+    [OrchardFeature("Associativy")]
+    public static class GraphContextExtensions
+    {
+        public static string Stringify(this IGraphContext graphContext)
+        {
+            return graphContext.GraphName + String.Join(" ", graphContext.ContentTypes);
+        }
     }
 }
