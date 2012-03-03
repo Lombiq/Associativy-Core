e5f66dcaba90ee3a30e8cec85c10855896c59ac5
piedone@pyrocenter.hu <piedone@pyrocenter.hu>
2012-03-03 20:35:00 +0100

    IGraphManager comments

--- GraphDiscovery/IGraphManager.cs
+++ GraphDiscovery/IGraphManager.cs
@@ -9,10 +9,23 @@ namespace Associativy.GraphDiscovery
     /// </summary>
     public interface IGraphManager : IDependency
     {
+        /// <summary>
+        /// Gets the last graph from the registered graphs that best matches the specified context
+        /// </summary>
+        /// <param name="graphContext">The graph context instance to match against</param>
         GraphDescriptor FindGraph(IGraphContext graphContext);
 
+        /// <summary>
+        /// Lists all registered graphs that match the specified context
+        /// </summary>
+        /// <param name="graphContext">The graph context instance to match against</param>
+        /// <returns></returns>
         IEnumerable<GraphDescriptor> FindGraphs(IGraphContext graphContext);
 
+        /// <summary>
+        /// Lists graphs that match the specified context from the registered graphs and makes them distinct by looking at the graph name
+        /// </summary>
+        /// <param name="graphContext">The graph context instance to match against</param>
         IEnumerable<GraphDescriptor> FindDistinctGraphs(IGraphContext graphContext);
     }
 }
