a000c335bd7cb2fcd50cdb2a5b2fc44828b57493
piedone@pyrocenter.hu <piedone@pyrocenter.hu>
2012-04-16 12:34:03 +0200

    Adding IGraphManager reference to AssociativyControllerBase

--- Controllers/AssociativyControllerBase.cs
+++ Controllers/AssociativyControllerBase.cs
@@ -1,11 +1,13 @@
 ﻿using System.Web.Mvc;
 using Associativy.Services;
+using Associativy.GraphDiscovery;
 
 namespace Associativy.Controllers
 {
     public abstract class AssociativyControllerBase : Controller
     {
         protected readonly IAssociativyServices _associativyServices;
+        protected readonly IGraphManager _graphManager;
         protected readonly IGraphService _graphService;
         protected readonly IMind _mind;
         protected readonly INodeManager _nodeManager;
@@ -14,6 +16,7 @@ namespace Associativy.Controllers
             IAssociativyServices associativyServices)
         {
             _associativyServices = associativyServices;
+            _graphManager = associativyServices.GraphManager;
             _graphService = associativyServices.GraphService;
             _mind = associativyServices.Mind;
             _nodeManager = associativyServices.NodeManager;
