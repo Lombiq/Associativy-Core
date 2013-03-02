b605d83c5956e9a20d6bccd8529f93fae4d608b9
Piedone <piedone@pyrocenter.hu>
2013-03-02 11:54:04 +0100

    AssociativyNodeTitleLabelPart beginnings

---
+++ Handlers/AssociativyNodeTitleLabelPartHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Associativy.Models;
+using Orchard.ContentManagement.Handlers;
+using Orchard.Environment.Extensions;
+using Orchard.ContentManagement;
+
+namespace Associativy.Handlers
+{
+    [OrchardFeature("Associativy")]
+    public class AssociativyNodeTitleLabelPartHandler : ContentHandler
+    {
+        protected override void GetItemMetadata(GetContentItemMetadataContext context)
+        {
+            var part = context.ContentItem.As<AssociativyNodeTitleLabelPart>();
+
+            if (part != null) context.Metadata.DisplayText = part.Title;
+        }
+    }
+}
---
+++ Models/Nodes/AssociativyNodeTitleLabelPart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Orchard.ContentManagement.Aspects;
+using Orchard.Environment.Extensions;
+
+namespace Associativy.Models
+{
+    [OrchardFeature("Associativy")]
+    public class AssociativyNodeTitleLabelPart : AssociativyNodeLabelPart, ITitleAspect
+    {
+        public string Title { get { return Label; } }
+    }
+}
