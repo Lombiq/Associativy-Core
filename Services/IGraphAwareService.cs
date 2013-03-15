96017096f4a9dcf260e47303c0a7433c93bc09fe
Piedone <piedone@pyrocenter.hu>
2013-03-15 16:48:39 +0100

    Typo

--- Services/IGraphAwareService.cs
+++ Services/IGraphAwareService.cs
@@ -2,7 +2,7 @@
 namespace Associativy.Services
 {
     /// <summary>
-    /// Markes a service that need the IGraphDescriptor instance of the graph to be used injected
+    /// Marks a service that needs the IGraphDescriptor instance of the graph to be used injected
     /// </summary>
     public interface IGraphAwareService
     {
