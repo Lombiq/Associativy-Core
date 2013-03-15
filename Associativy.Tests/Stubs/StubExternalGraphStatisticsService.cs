68a9e97b13350a68b5d5d09ee32804fcb9ea6b8c
Piedone <piedone@pyrocenter.hu>
2013-03-15 23:00:28 +0100

    Adding StubExternalGraphStatisticsService

---
+++ Associativy.Tests/Stubs/StubExternalGraphStatisticsService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Associativy.Models.Services;
+using Associativy.Services;
+
+namespace Associativy.Tests.Stubs
+{
+    public class StubExternalGraphStatisticsService : IExternalGraphStatisticsService
+    {
+        private readonly GraphInfo _graphInfo = new GraphInfo();
+
+
+        public IGraphInfo GetGraphInfo()
+        {
+            return _graphInfo;
+        }
+
+        public void SetNodeCount(int count)
+        {
+            _graphInfo.NodeCount = count;
+        }
+
+        public void SetConnectionCount(int count)
+        {
+            _graphInfo.ConnectionCount = count;
+        }
+
+        public void SetCentralNodeId(int id)
+        {
+            _graphInfo.CentralNodeId = id;
+        }
+
+
+        private class GraphInfo : IGraphInfo
+        {
+            public int NodeCount { get; set; }
+            public int ConnectionCount { get; set; }
+            public int CentralNodeId { get; set; }
+        }
+    }
+}
