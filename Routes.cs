b325542010e3aa4f1bd361d8720ac349a33a5c87
Piedone <piedone@pyrocenter.hu>
2013-05-01 12:58:57 +0200

    Adding Routes for NodeLabelAdminController so it's reachable from under Admin/
    --HG--
    branch : 1.7

---
+++ Routes.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Orchard.Mvc.Routes;
+
+namespace Associativy
+{
+    public class Routes : IRouteProvider
+    {
+        public void GetRoutes(ICollection<RouteDescriptor> routes)
+        {
+            foreach (var routeDescriptor in GetRoutes()) routes.Add(routeDescriptor);
+        }
+
+        public IEnumerable<RouteDescriptor> GetRoutes()
+        {
+            return new[]
+			{
+				new RouteDescriptor
+				{
+					Name = "AssociativyNodeLabelAdmin",
+					Route = new Route(
+						"Admin/Associativy/NodeLabel/{action}",
+						new RouteValueDictionary
+						{
+							{"area", "Associativy"},
+							{"controller", "AssociativyNodeLabelAdmin"},
+							{"action", "RefreshLabels"}
+						},
+						new RouteValueDictionary(),
+						new RouteValueDictionary
+						{
+							{"area", "Associativy"}
+						},
+						new MvcRouteHandler())
+				}
+			};
+        }
+    }
+}
