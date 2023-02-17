4a8cfbe208fe679d2546910cd5ef1a67405ac9a8
Dávid El-Saig <david.el-saig@lombiq.com>
2023-02-17 03:26:23 +0100

    Content handler to export the graph connections along with the content.

---
+++ Handlers/GraphMetaDataContentHandler.cs
@@ -0,0 +1,35 @@
+﻿using Associativy.GraphDiscovery;
+using Orchard.ContentManagement.Handlers;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace Associativy.Handlers
+{
+    public class GraphMetaDataContentHandler : ContentHandler
+    {
+        public const string ElementName = "GraphMetaData";
+
+        private readonly IGraphManager _graphManager;
+
+        public GraphMetaDataContentHandler(IGraphManager graphManager)
+        {
+            _graphManager = graphManager;
+        }
+
+        protected override void Exporting(ExportContentContext context)
+        {
+            var neighbourIds = _graphManager
+                .FindGraph(new GraphContext())
+                .Services
+                .ConnectionManager
+                .GetNeighbourIds(context.ContentItemRecord.Id, skip: 0, count: int.MaxValue)
+                .ToList();
+
+            if (!neighbourIds.Any()) return;
+
+            var element = context.Element(ElementName);
+            element.SetAttributeValue("RecordId", context.ContentItemRecord.Id);
+            foreach (var id in neighbourIds) element.Add(new XElement("ConnectionId", id));
+        }
+    }
+}
